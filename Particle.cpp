5e438b3844a263e49e640e9462412b5679e8ff7b
tanner18 <windsor@wtanner.de>
2018-11-14 16:17:41 +0100

    Fixed unit issue with relativity conversions

--- Particle.cpp
+++ Particle.cpp
@@ -6,7 +6,12 @@
 #include "Particle.h"
 #include <gsl/gsl_const_mksa.h> // for gsl physical constants
 
-double Particle::gamma() {return 1/(sqrt(1-velocity_*velocity_/(SPEED_OF_LIGHT*SPEED_OF_LIGHT)));}
+double Particle::gamma() {
+
+    double v = velocity_ * SPEED_OF_LIGHT;
+    return 1/(sqrt(1- v * v /(SPEED_OF_LIGHT*SPEED_OF_LIGHT)));
+
+}
 
 Particle::Particle(double mass, double velocity)
 :  mass_{1.0}, velocity_{1.0} {}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "Particle.h"
+#include <gsl/gsl_const_mksa.h>
 
 int main() {
 
     Particle G(.24, .70);
 
+
     std::cout << G.calculateMomentum() << std::endl;
+
+    std::cout << "Line: " << __LINE__ << std::endl;
     return 0;
 }
